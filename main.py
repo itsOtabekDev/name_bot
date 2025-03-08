3648f6a9ed314a0c1c176593d6605ab0af67e3b4
Otabek Abdusharipov <atabekabdusharipov@gmail.ru>
2025-03-08 08:49:12 +0500

    push2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from function import handle_text
-# TOKEN = os.getenv()
+import os
+TOKEN = os.getenv("TOKEN")
 def start_func(update, context):
     update.message.reply_text(text="Salom bu botga ism yozing bot sizga yozgan ismingizni manosini topib beradi!")
     print(update.message.text)
     print(context.bot)
     print(update.message.from_user)
 
-updater = Updater(token="7280611441:AAFnaRxjEnoIx_PUIiLr2TeUvGmCNqLEZ9s")
+updater = Updater(token=f"{TOKEN}")
 dispatcher = updater.dispatcher
 dispatcher.add_handler(CommandHandler('start', start_func))
 dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_text))
